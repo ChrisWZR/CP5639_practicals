aa477e4f5a3b70c954f368f79bafdefb20b1ab46
ChrisWZR <zhirong.wang@my.jcu.edu.au>
2023-04-17 16:23:17 +0800

    practical 5

---
+++ prac_05/programs.py
@@ -0,0 +1,48 @@
+# Input, Processing, Output
+# 1. Percentage change
+"""
+get original_num, change
+result = original_num * (1 + change)
+display original_num, change, result
+"""
+# original_num = float(input("Original: "))
+# change = float(input("Change: "))
+# result = original_num * (1 + change)
+# print(f"Original: {original_num}, Change: {change}, Result: {result}")
+
+# Decision Structures
+# 2. Time of day
+"""
+get time_of_day, coming_or_going
+if time_of_day < 12
+    time_str = before noon
+else
+    time_str = after noon
+
+if coming_or_going == "coming"
+    coming_str = Hi
+else
+    coming_str = Bye
+display f'It is {time_str} and you are {coming_or_going}. {coming_str}!'
+"""
+# time_of_day = int(input("The time of day (0-23 hours): "))
+# coming_or_going = input("Coming or going: ").lower()
+# if time_of_day < 12:
+#     time_str = "before noon"
+# else:
+#     time_str = "after noon"
+#
+# if coming_or_going == "coming":
+#     coming_str = "Hi"
+# else:
+#     coming_str = "Bye"
+#
+# print(f"It is {time_str} and you are {coming_or_going}. {coming_str}!")
+
+# 3. Coffee orders
+"""
+get coffee_type, coffee_size
+if coffee_type = black
+    
+display price
+"""
